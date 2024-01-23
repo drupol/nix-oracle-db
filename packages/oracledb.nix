e69b4804ca9edd4181917f49ff5223b0b09aaeec
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-01-23 13:34:24 +0100

    oracle-database: use `buildFHSEnv`

--- packages/oracledb.nix
+++ packages/oracledb.nix
@@ -1,65 +1,84 @@
 { lib
 , stdenv
 , fetchurl
+, buildFHSEnv
 
-, autoPatchelfHook
 , makeBinaryWrapper
 , rpmextract
 , libaio
 , alsa-lib
 }:
+let
+  oracle-database-base = stdenv.mkDerivation (finalAttrs: {
+    pname = "oracle-database-base";
+    version = "23c";
 
+    src = fetchurl {
+      url = "https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23c-1.0-1.el8.x86_64.rpm";
+      hash = "sha256-Exm818twbLcnUBy9mKvz85gKT9q+thOhq//HVpJcc3Q=";
+    };
+
+    nativeBuildInputs = [
+      makeBinaryWrapper
+      rpmextract
+    ];
+
+    buildInputs = [
+      stdenv.cc.cc.lib
+      libaio
+      alsa-lib
+    ];
+
+    unpackCmd = ''
+      mkdir ${finalAttrs.pname}-${finalAttrs.version} && pushd ${finalAttrs.pname}-${finalAttrs.version}
+      rpmextract $curSrc
+      popd
+    '';
+
+    dontPatchShebangs = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out $out/bin $lib/lib $bin/bin
+      cp -ar {etc,opt,usr} $out
+      cp -ar opt/oracle/product/23c/dbhomeFree/lib/* $lib/lib/
+      cp -ar opt/oracle/product/23c/dbhomeFree/bin/* $bin/bin/
+
+      runHook postInstall
+    '';
+
+    outputs = [ "bin" "out" "dev" "lib" ];
+  });
+
+  oracle-database-fhs = buildFHSEnv {
+    name = "oracle-database-fhs";
+
+    targetPkgs = pkgs: [
+      oracle-database-base
+      libaio
+    ];
+  };
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "oracle-database";
   version = "23c";
 
-  src = fetchurl {
-    url = "https://download.oracle.com/otn-pub/otn_software/db-free/oracle-database-free-23c-1.0-1.el8.x86_64.rpm";
-    hash = "sha256-Exm818twbLcnUBy9mKvz85gKT9q+thOhq//HVpJcc3Q=";
-  };
-
-  autoPatchelfIgnoreMissingDeps = true;
-
-  nativeBuildInputs = [
-    autoPatchelfHook
-    makeBinaryWrapper
-    rpmextract
-  ];
+  dontUnpack = true;
+  dontBuild = true;
 
   buildInputs = [
-    stdenv.cc.cc.lib
-    libaio
-    alsa-lib
+    oracle-database-fhs
   ];
-
-  preBuild = ''
-    addAutoPatchelfSearchPath "${placeholder "out"}/opt/oracle/product/23c/dbhomeFree/lib/"
-  '';
-
-  unpackCmd = ''
-    mkdir ${finalAttrs.pname}-${finalAttrs.version} && pushd ${finalAttrs.pname}-${finalAttrs.version}
-    rpmextract $curSrc
-    popd
-  '';
-
-  dontPatchShebangs = true;
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out $out/bin $lib/lib
-    cp -ar {etc,opt,usr} $out
+    mkdir -p $out/bin
+    ln -s ${oracle-database-fhs}/bin/sqlplus $out/bin/sqlplus
 
     runHook postInstall
   '';
-
-  postInstall = ''
-    for exe in "$out/opt/oracle/product/23c/dbhomeFree/bin/"* ; do
-      test -x "$exe" && makeWrapper $exe "$out/bin/$(basename "$exe")"
-    done
-  '';
-
-  outputs = [ "out" "dev" "lib" ];
 
   meta = {
     description = "Oracle database";
