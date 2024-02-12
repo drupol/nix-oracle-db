d94166b0dfbb7252a9b2df355c2444cd2e0e5dcc
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-02-12 10:34:49 +0100

    module: update

--- modules/oracle-database.nix
+++ modules/oracle-database.nix
@@ -16,20 +16,43 @@ in
 
   config = lib.mkIf cfg.enable {
     environment.etc."oratab".text = ''
+      free:/var/lib/oracle-database/oradata/free:N
+    '';
 
+    environment.etc."sysconfig/oracle-free-23c.conf".text = ''
+      # LISTENER PORT used Database listener, Leave empty for automatic port assignment
+      LISTENER_PORT=1521
+
+      # Character set of the database
+      CHARSET=AL32UTF8
+
+      # Database file directory
+      # If not specified, database files are stored under Oracle base/oradata
+      DBFILE_DEST=/var/lib/oracle-database/oradata
+
+      # DB Domain name
+      DB_DOMAIN=
+
+      # SKIP Validations, memory, space
+      SKIP_VALIDATIONS=false
     '';
 
     systemd.services.oracle-database = {
       description = "Oracle Database";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
-
+      preStart = ''
+        mkdir -p $STATE_DIRECTORY/oradata
+        cat /etc/oratab
+        cat /etc/sysconfig/oracle-free-23c.conf
+      '';
       serviceConfig = {
-        ExecStart = "${cfg.package}/bin/dbstart";
+        ExecStart = "${cfg.package}/bin/dbstart /var/lib/oracle-database/oradata";
         StateDirectory = "oracle-database";
         DynamicUser = true;
         PrivateTmp = "yes";
         Restart = "on-failure";
+        Environment = [];
       };
     };
   };
